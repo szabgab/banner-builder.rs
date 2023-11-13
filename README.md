d7b9773049c179b0db5669a72da1b2d515714aba
Gabor Szabo <gabor@szabgab.com>
2023-11-13 17:14:10 +0200

    add test

--- README.md
+++ README.md
@@ -11,11 +11,6 @@ cargo run --bin babu test/hello_world.yaml hello_world.png
 
 Add test
 ```
-cargo run --bin babu hello_world.yaml hello_world.png
-Expected output: Could not open file 'hello_world.yaml', error: No such file or directory (os error 2)
-```
-
-```
 cargo run --bin babu examples/hello_world.yaml hello_world.png
 ```
 
---
+++ tests/test_babu.rs
@@ -0,0 +1,28 @@
+use std::process::Command;
+
+#[cfg(test)]
+mod tests {
+    use super::run;
+
+    #[test]
+    fn test_babu() {
+        let (exit, stdout, stderr) = run("cargo run --bin babu hello_world.yaml hello_world.png");
+        assert_eq!(exit, 1);
+        assert_eq!(stdout, "");
+        assert!(stderr.contains("Could not open file 'hello_world.yaml', error: No such file or directory"));
+    }
+}
+
+fn run(command: &str) -> (i32, String, String) {
+    let parts: Vec<&str> = command.split(' ').collect();
+    
+    let cmd = &parts[0];
+    let args = &parts[1..parts.len()];
+
+    let result = Command::new(cmd)
+        .args(args)
+        .output()
+        .expect("ls command failed to start");
+
+    (result.status.code().unwrap(), std::str::from_utf8(&result.stdout).unwrap().to_owned(), std::str::from_utf8(&result.stderr).unwrap().to_owned())
+}
