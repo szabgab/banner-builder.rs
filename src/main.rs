19ae0dd791e9e3735ab3528c03e819d1d77fe2ea
Gabor Szabo <gabor@szabgab.com>
2023-11-13 07:04:34 +0200

    move code to crate image from code-maven.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+
 fn main() {
     println!("Hello, world!");
+    let filename = "hello.png".to_string();
+    let path = &std::path::Path::new(&filename).to_path_buf();
+    banner_builder::draw_image(path, "Hello World");
 }
