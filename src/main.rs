2a976dd2b33bb06e4b80c1bf27ce561baa79258b
Gabor Szabo <gabor@szabgab.com>
2023-11-23 09:30:34 +0200

    extract read_yaml_file

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ fn main() {
         std::process::exit(1);
     }
     let yaml_file = &args[1];
+    let banner = read_yaml_file(yaml_file);
+    let filename = &args[2];
+
+    let path = &std::path::Path::new(&filename).to_path_buf();
+    banner_builder::draw_image(&banner, path);
+}
+
+fn read_yaml_file(yaml_file: &String) -> banner_builder::Banner {
     let banner: banner_builder::Banner = match std::fs::File::open(yaml_file) {
         Ok(file) => match serde_yaml::from_reader(file) {
             Ok(content) => content,
@@ -18,8 +26,5 @@ fn main() {
             std::process::exit(1);
         }
     };
-    let filename = &args[2];
-
-    let path = &std::path::Path::new(&filename).to_path_buf();
-    banner_builder::draw_image(&banner, path);
+    banner
 }
