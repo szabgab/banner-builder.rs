b1d1ad5d6aaa9297e6f1e418e453338c41edd39d
Gabor Szabo <gabor@szabgab.com>
2024-05-31 19:30:34 +0200

    make main return a Result

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use std::error::Error;
 use std::path::PathBuf;
 
-fn main() {
-    simple_logger::init_with_env().unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_env()?;
     log::info!("Starting banner builder");
 
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
-        eprintln!("{} config.yaml image.png", &args[0]);
-        std::process::exit(1);
+        return Err(Box::<dyn Error>::from(format!(
+            "{} config.yaml image.png",
+            &args[0]
+        )));
     }
     let yaml_file = PathBuf::from(&args[1]);
     let filename = &args[2];
@@ -16,4 +19,6 @@ fn main() {
     let path = &std::path::Path::new(&filename).to_path_buf();
     let root = yaml_file.parent().unwrap();
     banner_builder::draw_image(&banner, root, path);
+
+    Ok(())
 }
